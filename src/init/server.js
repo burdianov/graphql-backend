8e1cfea4d1529cb7d34f3788a234a4388fe1a583
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-24 19:05:58 +0400

    refactoring

--- src/init/server.js
+++ src/init/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 
-import { schema } from './../schema';
-import { resolvers } from './../resolvers';
+import schema from '../schema.graphql';
+import { resolvers } from '../resolvers';
 
 const app = express();
 
