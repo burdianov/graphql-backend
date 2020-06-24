628b4a010e9fc91b3720be0fd1914047abf7075f
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-24 17:45:47 +0400

    first typeDef

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
+const schema = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello client'
+  }
+};
+
+const PORT = 4000;
+
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers
+});
+
+server.applyMiddleware({ app });
+
+app.listen({ port: PORT }, () => {
+  console.log(`Listening on http://localhost:${PORT}${server.graphqlPath}`);
+});
