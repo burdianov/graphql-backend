564183f429372a4c89457190b7488124892bb058
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-24 20:45:45 +0400

    refactoring

--- src/model.js
+++ src/model.js
@@ -1,12 +1,35 @@
-const booksBD = [
-  {
-    title: 'Harry Potter',
-    author: 'J.K. Rowling'
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton'
-  }
+const initialDB = [
+  [
+    'ID-1',
+    {
+      title: 'Harry Potter',
+      author: 'J.K. Rowling'
+    }
+  ],
+  [
+    'ID-2',
+    {
+      title: 'Jurassic Park',
+      author: 'Michael Crichton'
+    }
+  ]
 ];
 
-export const getBooks = () => booksBD;
+const booksBD = new Map(initialDB);
+
+export const getBooks = () => {
+  const books = [];
+
+  booksBD.forEach((value, key) => {
+    console.log(key);
+    console.log(value);
+    const currentBook = {
+      id: key,
+      ...value
+    };
+    books.push(currentBook);
+  });
+  return books;
+};
+
+console.log(getBooks());
